c85fc68060a19f4cb87283b1f3478a215943e06e
Jx1126 <147470595+Jx1126@users.noreply.github.com>
2024-12-09 12:28:25 +0800

    Added movement support to the player

--- Task 1 Maze/Assets/Scripts/PlayerControl.cs
+++ Task 1 Maze/Assets/Scripts/PlayerControl.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
 
-    
-    void Start()
-    {
-        
-    }
+    public float speed = 15f;
 
     void Update()
     {
-      
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, 0, vertical).normalized;
+        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+
     }
 }
